5cd37b4b98bfff5448bc51a0c18ab0775c85fc22
Trimurthulu <trimurthuluposimsetti.com>
2020-06-14 12:27:51 +0530

    First

---
+++ src/test/java/com/mthree/StockMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mthree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
